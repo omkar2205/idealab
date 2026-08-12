f61efd1068824d70d2f8790206133b8d60e93ac2
P Omkareshwar Reddy <coolomkar11@gmail.com>
2026-08-12 14:41:31 +0530

    Switch IdeaLab frontend to Supabase backend

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.IDEALAB_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbxzQTl9zjhjWId61wv7zaWutWJMSq2xgRXJXNFpRwD_N9iRj444U4rosXQ_kGXkZQ5r/exec',
+  API_URL: 'https://raspnyrkjhgsydtcdrta.supabase.co/functions/v1/idealab-gateway',
   APP_NAME: 'IdeaLab',
   REQUEST_TIMEOUT_MS: 25000
 });
