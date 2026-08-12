c9091a49ffff2b58e716962f19e899a23c18f7c6
P Omkareshwar Reddy <coolomkar11@gmail.com>
2026-08-12 17:34:53 +0530

    Add IdeaLab insights endpoint

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.IDEALAB_CONFIG = Object.freeze({
   API_URL: 'https://raspnyrkjhgsydtcdrta.supabase.co/functions/v1/idealab-gateway',
+  INSIGHTS_API_URL: 'https://raspnyrkjhgsydtcdrta.supabase.co/functions/v1/idealab-insights',
   APP_NAME: 'IdeaLab',
   REQUEST_TIMEOUT_MS: 25000
 });
